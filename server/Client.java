22aef8d4ef490fd4a15e53f8193d5c87fa56dcce
ben911laten <99298839+ben911laten@users.noreply.github.com>
2022-12-23 22:58:10 +0200

    Add files via upload

---
+++ server/Client.java
@@ -0,0 +1,45 @@
+package server;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	private static Client instance = null;
+	private static Socket server = null;
+	private static DataInputStream in = null;
+	private static DataOutputStream out =null;
+	
+	public static Client getInstance() throws IOException{
+		if(instance == null)
+			instance = new Client();
+		return instance;
+	}
+	
+	private Client() throws IOException{
+		server = new Socket("41.236.137.50",25565);
+		in = new DataInputStream(server.getInputStream());
+		out = new DataOutputStream(server.getOutputStream());
+	}
+	
+	public void reconnect() {
+		try {
+			instance = new Client();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String send(String s) {
+		try {
+			out.writeUTF(s);
+			return in.readUTF();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+			reconnect();
+			return "Request failed, please retry,";
+		}
+	}
+}
